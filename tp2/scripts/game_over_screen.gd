a8f371adb474767db36754c35613d36bc66f9f35
Rada Maiz <rada@gmail.com>
2025-11-05 03:07:23 -0500

    init 5

--- tp2/scripts/game_over_screen.gd
+++ tp2/scripts/game_over_screen.gd
@@ -3,9 +3,12 @@ extends Control
 
 func _on_restart_button_pressed() -> void:
 	get_tree().reload_current_scene()
-
+	
 func set_score(value):
 	$Panel/Score.text = "Score: " + str(value)
 	
 func set_high_score(value):
 	$Panel/HighScore.text = "Hi-Score: " + str(value)
+
+func _on_go_main_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
